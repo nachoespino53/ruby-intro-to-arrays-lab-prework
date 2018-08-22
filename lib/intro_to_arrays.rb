69a360b1c96b720ad67a745a94a73c948eef4f4e
Ignacio R Espino <irespino@capozios.com>
2018-08-22 03:44:33 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,9 +1,9 @@
 def instantiate_new_array
-  return []
+  array = []
 end
 
 def array_with_two_elements
-  return [element1, element2]
+  array = [element1, element2]
 end
 
 def first_element(arg)
